4ac05aebaceacfd29bcde1c3c45406914c797e81
Viazel <timeo.morsilli@gmail.com>
2026-02-27 20:49:48 +0100

    feat: profile picture and name

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,8 @@
 export default defineAppConfig({
   global: {
     picture: {
-      dark: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      light: 'https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=1480&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      dark: 'https://avatars.githubusercontent.com/u/97253267?v=4',
+      light: 'https://avatars.githubusercontent.com/u/97253267?v=4',
       alt: 'My profile picture'
     },
     meetingLink: 'https://cal.com/',
@@ -26,18 +26,13 @@ export default defineAppConfig({
     credits: `Built with Nuxt UI • © ${new Date().getFullYear()}`,
     colorMode: false,
     links: [{
-      'icon': 'i-simple-icons-discord',
-      'to': 'https://go.nuxt.com/discord',
-      'target': '_blank',
-      'aria-label': 'Nuxt on Discord'
-    }, {
       'icon': 'i-simple-icons-x',
-      'to': 'https://go.nuxt.com/x',
+      'to': 'https://x.com/TMorsilli',
       'target': '_blank',
       'aria-label': 'Nuxt on X'
     }, {
       'icon': 'i-simple-icons-github',
-      'to': 'https://github.com/nuxt/ui',
+      'to': 'https://github.com/Viazel',
       'target': '_blank',
       'aria-label': 'Nuxt UI on GitHub'
     }]
