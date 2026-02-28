53af847f21eed5163032d7a6a4f5f0d404cc0785
Viazel <timeo.morsilli@gmail.com>
2026-02-28 14:26:59 +0100

    feat: organize my portfolio

--- app/app.config.ts
+++ app/app.config.ts
@@ -6,7 +6,7 @@ export default defineAppConfig({
       alt: 'My profile picture'
     },
     meetingLink: 'https://cal.com/',
-    email: 'ui-pro@nuxt.com',
+    email: 'timeo.morsilli@gmail.com',
     available: true
   },
   ui: {
@@ -23,7 +23,7 @@ export default defineAppConfig({
     }
   },
   footer: {
-    credits: `Built with Nuxt UI • © ${new Date().getFullYear()}`,
+    credits: `Viazel • © ${new Date().getFullYear()}`,
     colorMode: false,
     links: [{
       'icon': 'i-simple-icons-x',
